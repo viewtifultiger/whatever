4cd3f104478c60168e62706771d3b72461ca47dc
viewtifultiger <viewtifultiger@gmail.com>
2025-03-07 18:12:00 -0800

    3/7

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-nav-bar',
-  imports: [RouterLink],
+  imports: [
+    RouterLink,
+    MatToolbarModule,
+    MatIconModule,
+  ],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
